06e6c7e6b07ee40d5e472880bb562160e322a4a4
Paulo Silva <pauloalmeida1288@gmail.com>
2024-05-10 14:17:07 +0100

    Exercício extra, leitura de ficheiros adicionado

---
+++ CharSets/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CharSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<List<string>> list_of_chars = new List<List<string>>();
+
+            Console.WriteLine(args.Length);
+            for(int i = 0; i < args.Length; i++)
+            {
+                string s;
+                using StreamReader file = new StreamReader(args[i]);
+                List<string> chars = new List<string>();
+                while ((s = file.ReadLine()) != null)
+                {
+                    chars.Add(s);
+                }
+                list_of_chars.Add(chars);
+            }
+        }
+    }
+}
