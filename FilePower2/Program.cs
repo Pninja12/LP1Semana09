9b5f339463d73f323678182ae50735c54441fe2f
Paulo Silva <pauloalmeida1288@gmail.com>
2024-05-10 14:03:12 +0100

    Exercício 6 concluído

---
+++ FilePower2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.IO;
+
+namespace FilePower2
+{
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            string empty = " ";
+
+            Console.WriteLine(args[0]);
+            using StreamWriter file = new StreamWriter(args[0]);
+
+            while(empty != "")
+            {
+                empty = Console.ReadLine();
+                if (empty != "")
+                file.WriteLine(empty);
+            }
+        }
+    }
+}
