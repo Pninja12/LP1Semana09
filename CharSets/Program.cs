3c7fabba3ae7a3c0dd8e7ef18243db2b5bea26c7
Paulo Silva <pauloalmeida1288@gmail.com>
2024-05-10 14:27:27 +0100

    Exercício extra, leitura de vários ficheiros concluída

--- CharSets/Program.cs
+++ CharSets/Program.cs
@@ -10,7 +10,6 @@ namespace CharSets
         {
             List<List<string>> list_of_chars = new List<List<string>>();
 
-            Console.WriteLine(args.Length);
             for(int i = 0; i < args.Length; i++)
             {
                 string s;
@@ -19,8 +18,20 @@ namespace CharSets
                 while ((s = file.ReadLine()) != null)
                 {
                     chars.Add(s);
+                    if (s.Length > 2)
+                    {
+                        Console.Write("Error: File with 2 chars in same line");
+                        Environment.Exit(1);
+                    }
                 }
                 list_of_chars.Add(chars);
+            }
+            foreach (List<string> a in list_of_chars)
+            {
+                foreach (string b in a)
+                {
+                    Console.WriteLine(b);
+                }
             }
         }
     }
